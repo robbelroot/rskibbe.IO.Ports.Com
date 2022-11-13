9f8239a80ef9f0c1349d24ffdb798c869f1c8ba9
Robert Skibbe <skibbe.robert@web.de>
2022-11-13 09:41:40 +0100

    removed abstract and renamed base class

---
+++ rskibbe.IO.Ports.Com/System/SystemComPortsBase.cs
@@ -0,0 +1,23 @@
+﻿using rskibbe.IO.Ports.Com.ValueObjects;
+
+namespace rskibbe.IO.Ports.Com.System;
+
+public abstract class SystemComPortsBase : ISystemComPorts
+{
+
+    public List<string> ExistingPorts { get; }
+
+    public abstract Task<IEnumerable<byte>> ListUsedPortIdsAsync();
+
+    public abstract Task<IEnumerable<string>> ListUsedPortNamesAsync();
+
+    public SystemComPortsBase()
+    {
+        ExistingPorts = new List<string>();
+    }
+
+    public event EventHandler<ComPortEventArgs>? SystemComPortAdded;
+
+    public event EventHandler<ComPortEventArgs>? SystemComPortRemoved;
+
+}
